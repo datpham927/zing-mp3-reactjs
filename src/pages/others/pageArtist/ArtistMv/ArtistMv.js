78b781d4897c6dfe909ba6bfb96614b6cf618d1d
dat pham <datp927@gmail.com>
2023-01-12 14:20:06 +0700

    up

--- src/pages/others/pageArtist/ArtistMv/ArtistMv.js
+++ src/pages/others/pageArtist/ArtistMv/ArtistMv.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 
 import Container from '~/components/container/Container';
 import ItemVideo from '~/components/item/ItemVideo/ItemVideo';

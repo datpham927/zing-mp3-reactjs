433a3a9d10e6a966242fba9855e421e969af9f98
dat pham <datp927@gmail.com>
2023-02-16 18:30:15 +0700

    upup

--- src/pages/primary/Home/HomeGallery/HomeGallery.js
+++ src/pages/primary/Home/HomeGallery/HomeGallery.js
@@ -12,35 +12,27 @@ function HomeGallery({ data }) {
 
     useEffect(() => {
         let interval;
-        const listImg = document?.querySelectorAll(`.${cx('item-image').replace('+', 'l')}`);
+        const listImg = document?.querySelectorAll(`${cx('item-image')}`);
+        console.log(listImg);
         const autoChange = () => {
             let numberIndex = 0;
             const change = () => {
                 listImg.forEach((item, i) => {
                     if (i === numberIndex) {
-                        item.classList.replace(`${cx('second').replace('+', 'l')}`, `${cx('first').replace('+', 'l')}`);
+                        item.classList.replace(`${cx('second')}`, `${cx('first')}`);
                     } else if (i === numberIndex + 1) {
-                        item.classList.replace(`${cx('third').replace('+', 'l')}`, `${cx('second').replace('+', 'l')}`);
+                        item.classList.replace(`${cx('third')}`, `${cx('second')}`);
                     } else if (i === numberIndex + 2) {
-                        item.classList.replace(`${cx('four').replace('+', 'l')}`, `${cx('third').replace('+', 'l')}`);
+                        item.classList.replace(`${cx('four')}`, `${cx('third')}`);
                     } else {
-                        item.classList.replace(`${cx('first').replace('+', 'l')}`, `${cx('four').replace('+', 'l')}`);
+                        item.classList.replace(`${cx('first')}`, `${cx('four')}`);
                     }
                     if (numberIndex === listImg?.length - 2) {
-                        listImg[0].classList.replace(
-                            `${cx('four').replace('+', 'l')}`,
-                            `${cx('third').replace('+', 'l')}`,
-                        );
+                        listImg[0].classList.replace(`${cx('four')}`, `${cx('third')}`);
                     }
                     if (numberIndex === listImg?.length - 1) {
-                        listImg[0].classList.replace(
-                            `${cx('third').replace('+', 'l')}`,
-                            `${cx('second').replace('+', 'l')}`,
-                        );
-                        listImg[1].classList.replace(
-                            `${cx('four').replace('+', 'l')}`,
-                            `${cx('third').replace('+', 'l')}`,
-                        );
+                        listImg[0].classList.replace(`${cx('third')}`, `${cx('second')}`);
+                        listImg[1].classList.replace(`${cx('four')}`, `${cx('third')}`);
                     }
                 });
                 numberIndex++;
@@ -68,7 +60,7 @@ function HomeGallery({ data }) {
                             'item-image',
                             index === 0 ? 'first' : index === 1 ? 'second' : index === 2 ? 'third' : 'four',
                             ' l-4 m-6 c-12',
-                        ).replace('+', 'l')}
+                        )}
                         onLoad={() => setHeightImg(itemRef.current.offsetHeight)}
                     >
                         <Link to={item.link}>
